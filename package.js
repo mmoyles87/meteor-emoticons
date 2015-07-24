0b2d72cc19c78f4636a28063dc230a3b96fa703c
Matt Moyles <mattmoyles@gmail.com>
2015-07-24 11:43:59 -0400

    Added missing images and changed permissions on assets to 0644 closes #1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mattimo:emoticons',
-  version: '0.0.1',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Helpers for image based emoticons in your application. You can easily add your own icons.',
   // URL to the Git repository containing the source code for this package.
